708ed61eae0ad2786c1e4190c9045a4f20357670
vikasreddy789 <vikas.reddy@snapwiz.com>
2017-02-12 10:22:43 +0530

    Added -- offline support, using sw-precache

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/* Register a service worker */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js', { scope: '/' })
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
